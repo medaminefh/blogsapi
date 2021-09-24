a59c384b84097ccb5a9b5514359d763b18fb2986
med-amine-fh <fhal.mohamed.amine@gmail.com>
2021-09-24 19:24:01 +0100

    🆕 add morgan and helmet

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 require("dotenv").config();
 
 const cors = require("cors");
+const morgan = require('morgan');
+const helmet = require('helmet');
 const PORT = process.env.PORT || 5000;
 const express = require("express");
+const {errorHandler, notFound} = require("./middleware/auth")
 const app = express();
 
-app.use(express.json({ limit: "50mb" }));
+app.use(morgan('dev'));
+app.use(helmet());
+
+app.use(express.json({limit: '50mb'}));
 app.use(
-  express.urlencoded({ limit: "50mb", parameterLimit: 100000, extended: true })
+  express.urlencoded({limit: '50mb', extended: true })
 );
 
 app.use(cors());
@@ -24,6 +30,9 @@ app.get("/", (req, res) => {
 app.use("/api/blogs", require("./routes/blog"));
 
 app.use("/login", require("./routes/login"));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log("Server is running on port ", PORT);
